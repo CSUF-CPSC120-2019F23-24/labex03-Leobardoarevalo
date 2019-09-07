5f3cbfc3cfd04eda4d5a12961ec022944b343add
Leobardo.arevalo25 <leobardo.arevalo25@cs104u-3b.acad.fullerton.edu>
2019-09-07 15:06:45 -0700

    finished lab3

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,16 @@
 // This program calculates a person's height in feet (') and inches (").
+#include <iostream>
+
+int main()
+{
+  int height, inches, feet;
+std::cout << "what is the height in feet:";
+std::cin >> height;
+
+feet = height / 12;
+inches = height % 12;
+
+std::cout << "that person is " << feet <<"'"<< inches << "\""<< '\n';
+
+  return 0;
+}
