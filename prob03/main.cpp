7c13aaca7481c649fc3691a2f4a75d00f2f1f6bc
Leobardo.arevalo25 <leobardo.arevalo25@cs104u-3b.acad.fullerton.edu>
2019-09-14 13:23:53 -0700

    corrected

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -3,14 +3,15 @@
 
 int main()
 {
-  int height, inches, feet;
+int height, inches, feet;
 std::cout << "what is the height in feet:";
 std::cin >> height;
-
+// division gives you the amount of feet
 feet = height / 12;
+// modules gives me the remainder
 inches = height % 12;
 
 std::cout << "that person is " << feet <<"'"<< inches << "\""<< '\n';
 
-  return 0;
+return 0;
 }
